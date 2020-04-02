185dd029314d04b1a4fc110d94ee2e25e47d9b04
BittorHM <61824737+BittorHM@users.noreply.github.com>
2020-04-02 13:47:12 +0200

    Falta pasar el consent code

--- app/src/main/java/com/welie/blessedexample/MainActivity.java
+++ app/src/main/java/com/welie/blessedexample/MainActivity.java
@@ -102,6 +102,8 @@ public class MainActivity extends AppCompatActivity {
                 measurementValue3.setText("Scaning");
                 BluetoothHandler.getInstance(getApplicationContext());
                 registerReceiver(scaleDataReceiver, new IntentFilter("ScaleMeasurement"));
+                Intent intent = new Intent(MainActivity.this, ScaleActivity.class);
+                startActivity(intent);
             }
         });
 
